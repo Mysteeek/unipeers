a108325aefbd532f381ac1a4ce60ce83fc4f6a29
mysteeek <chinedunkwonta676@gmail.com>
2025-08-11 23:30:38 +0100

    ui design for events

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,35 @@
+import { themeColors } from "@/utils/theme.utils"
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5"
+import MaterialIcons from "@expo/vector-icons/MaterialIcons"
 import { Tabs } from "expo-router"
 
 export default function _Layout () {
     return (
-        <Tabs>
+        <Tabs screenOptions={{
+            tabBarActiveTintColor: themeColors.darkGreen
+        }}>
             <Tabs.Screen
             name="index"
             options={{
+                title: "Feeds",
                 headerShown: false,
+                tabBarIcon: ({ color}) => <MaterialIcons name="dynamic-feed" size={34} color={color} />
             }}/>
 
             <Tabs.Screen
             name="profile"
             options={{
+                title: "Profile",
                 headerShown: false,
+                tabBarIcon: ({ color}) => <FontAwesome5 name="user" size={34} color={color} />
+            }}/>
+
+            <Tabs.Screen
+            name="saved"
+            options={{
+                title: "saved",
+                headerShown: false,
+                tabBarIcon: ({ color}) => <MaterialIcons name="bookmark" size={34} color={color} />
             }}/>
             
         </Tabs>
